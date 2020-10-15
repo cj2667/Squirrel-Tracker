9760c96263272037382d19a2a11fe681df13d2f1
Cheng Ji <cj2667@columbia.edu>
2020-10-15 02:58:45 +0000

    Delete Print test code in import_squirrels_data

--- squirrels/management/commands/import_squirrel_data.py
+++ squirrels/management/commands/import_squirrel_data.py
@@ -1,20 +1,13 @@
 from django.core.management.base import BaseCommand
 import csv
-
-
+  
 class Command(BaseCommand):
     help = 'Get squirrels information'
-
     def add_arguments(self, parser):
         parser.add_argument('squirrel_file', help = 'file contain squirrel details')
-    
     def handle(self, *args, **options):
         file_ = options['squirrel_file']
-        
-        with open(file_) as fp:
-            reader = csv.DictReader(fp)
-
-            for item in reader:
-                print(item)
+        msg = f'You are importing from {file_}'
+        self.stdout.write(self.style.SUCCESS(msg))
     
 
