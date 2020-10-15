8d16ce78b63e1356e299ba34c4c5580515d5b494
Cheng Ji <cj2667@columbia.edu>
2020-10-15 19:44:25 +0000

    Exciting news! Added 3000 objs with six attributes

--- squirrels/management/commands/import_squirrel_data.py
+++ squirrels/management/commands/import_squirrel_data.py
@@ -1,13 +1,26 @@
 from django.core.management.base import BaseCommand
 import csv
-  
+from squirrels.models import Squirrel
+
 class Command(BaseCommand):
     help = 'Get squirrels information'
+
     def add_arguments(self, parser):
         parser.add_argument('squirrel_file', help = 'file contain squirrel details')
+
     def handle(self, *args, **options):
         file_ = options['squirrel_file']
-        msg = f'You are importing from {file_}'
+        with open(file_) as fp:
+            reader = csv.DictReader(fp)
+            for item in reader:
+                obj = Squirrel()
+                obj.Latitude = item["X"]
+                obj.Longitude = item["Y"]
+                obj.Unique_Squirrel_ID = item["Unique Squirrel ID"]
+                obj.Shift = item["Shift"]
+                obj.Date = item["Date"]
+                obj.Age = item["Age"]
+                obj.save()
+        msg = f"You are importing from {file_}"
         self.stdout.write(self.style.SUCCESS(msg))
-    
 
