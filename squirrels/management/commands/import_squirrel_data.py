25a0a4c02493f3660d48a77bc76d429bf2570b0c
Cheng Ji <cj2667@columbia.edu>
2020-10-22 18:18:10 -0400

    Change import_squirrel_data file back to original state

--- squirrels/management/commands/import_squirrel_data.py
+++ squirrels/management/commands/import_squirrel_data.py
@@ -13,27 +13,19 @@ class Command(BaseCommand):
         file_ = options['squirrel_file']
         with open(file_) as fp:
             reader = csv.DictReader(fp)
-            data = list(reader)
-        new_l = []
-        id_l = []
-        for i in data:
-            if i["Unique Squirrel ID"] not in id_l:
-                new_l.append(i)
-                id_l.append(i["Unique Squirrel ID"])
-        data = new_l
-        for item in data:
-            obj = Squirrel()
-            obj.Latitude = item["X"]
-            obj.Longitude = item["Y"]
-            obj.Unique_Squirrel_ID = item["Unique Squirrel ID"]
-            obj.Shift = item["Shift"]
-            obj.Date = datetime.strptime(item["Date"],'%m%d%Y').date()
-            obj.Age = item["Age"]
-            obj.Running = item["Running"]
-            obj.Chasing = item["Chasing"]
-            obj.Climbing = item["Climbing"]
-            obj.Eating = item["Eating"]
-            obj.Foraging = item["Foraging"]
-            obj.save()
+            for item in reader:
+                obj = Squirrel()
+                obj.Latitude = item["X"]
+                obj.Longitude = item["Y"]
+                obj.Unique_Squirrel_ID = item["Unique Squirrel ID"]
+                obj.Shift = item["Shift"]
+                obj.Date = datetime.strptime(item["Date"],'%m%d%Y').date()
+                obj.Age = item["Age"]
+                obj.Running = item["Running"]
+                obj.Chasing = item["Chasing"]
+                obj.Climbing = item["Climbing"]
+                obj.Eating = item["Eating"]
+                obj.Foraging = item["Foraging"]
+                obj.save()
         msg = f"You are importing from {file_}"
         self.stdout.write(self.style.SUCCESS(msg))
